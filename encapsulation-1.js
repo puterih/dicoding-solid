ef43145ac3750a8b654a91dc8ca5187943a95078
puteri husnul k <husnulputeri@gmail.com>
2021-07-07 13:21:51 +0700

    Create encapsulation-1.js

---
+++ encapsulation-1.js
@@ -0,0 +1,14 @@
+class DataRepository {
+    constructor() {
+        this._data = [];
+    }
+
+    setData() {
+        const response = /* tons of logical code to retrieve data */
+        this._data = response.data;
+    }
+
+    get data () {
+        return this._data;
+    }
+}
