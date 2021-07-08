597a963027a66ddaee46da793f1856599f8dee7e
puteri husnul k <husnulputeri@gmail.com>
2021-07-08 10:55:46 +0700

    Create objectOrientedProgramming-3.js

---
+++ objectOrientedProgramming-3.js
@@ -0,0 +1,8 @@
+class Kucing {
+    constructor(color, height, length, weight) {
+        this._color = color;
+        this._height = height;
+        this._length = length;
+        this._weight = weight;
+    }
+}
