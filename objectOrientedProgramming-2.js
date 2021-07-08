34fa449d03ad5f23a45efc2bf85db068f18b8bd9
puteri husnul k <husnulputeri@gmail.com>
2021-07-08 10:47:24 +0700

    Create objectOrientedProgramming-2.js

---
+++ objectOrientedProgramming-2.js
@@ -0,0 +1,11 @@
+const main = () => {
+    const persian = new Kucing();
+    persian.color = "Putih";
+    persian.height = 24.0;
+    persian.length = 46.0;
+    persian.weight = 2.0;
+    const bengal = new Kucing("Coklat", 22.0, 39.0, 2.3);
+    const anggora = new Kucing("Abu-abu", 25.0, 41.0, 2.4);
+};
+
+main();
