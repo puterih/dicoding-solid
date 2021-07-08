2495774c75458aa95a93fbd06f16a6f8f1d9d31c
puteri husnul k <husnulputeri@gmail.com>
2021-07-08 22:04:16 +0700

    Create inheritance-1.js

---
+++ inheritance-1.js
@@ -0,0 +1,12 @@
+// mewarisi satu class
+class Animal {
+    walk() {
+        console.log(`${this.constructor.name} walk!`);
+    }
+}
+
+class Cat extends Animal {
+    walk() {
+        console.log(`${this.constructor.name} walked!`);
+    }
+}
