4109da7d3ee1beedbfb266c1c7500999ac723186
puteri husnul k <husnulputeri@gmail.com>
2021-07-09 11:43:47 +0700

    Update encapsulation-1.js

--- encapsulation-1.js
+++ encapsulation-1.js
@@ -5,10 +5,10 @@ class DataRepository {
 
     setData() {
         const response = /* tons of logical code to retrieve data */
-        this._data = response.data;
+        this._data - response.data;
     }
 
-    get data () {
+    get data() {
         return this._data;
-    }
+    }  
 }
