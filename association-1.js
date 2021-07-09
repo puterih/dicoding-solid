63b503f7c10273da825decfa8bf06ba30ae9abcc
puteri husnul k <husnulputeri@gmail.com>
2021-07-09 14:57:38 +0700

    Create association-1.js

---
+++ association-1.js
@@ -0,0 +1,17 @@
+// Kardinalitas
+// one-to-many
+class Seller {
+    constructor(id, name) {
+        this._id = id;
+        this._name = name;
+    }
+}
+
+class User {
+    constructor(id, name, sellers) {
+        this._id = id;
+        this._name = name;
+        this._sellers = sellers;
+    }
+}
+
