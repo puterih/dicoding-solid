49812a5a3be4c7cbb84997afcb7f87b02fa92eca
puteri husnul k <husnulputeri@gmail.com>
2021-07-09 12:59:24 +0700

    Create polymorphism-1.js

---
+++ polymorphism-1.js
@@ -0,0 +1,20 @@
+// menerapkan method overriding 
+class Animal {
+    walk() {
+        console.log(`${this.constructor.name} walk!`);
+    }
+}
+
+class Cat extends Animal {
+    walk() {
+        super.walk();
+        console.log(`Yeay! ${this.constructor.name} walked!`);
+    }
+}
+
+const main = () => {
+    const cat = new Cat();
+    cat.walk()
+};
+
+main();
