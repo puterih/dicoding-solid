461e9cb0077ba2193fb79f057ccbfcbde25e56f3
puteri husnul k <husnulputeri@gmail.com>
2021-07-12 05:20:12 +0700

    Create singleResponsibilityPrinciple-1.js

---
+++ singleResponsibilityPrinciple-1.js
@@ -0,0 +1,38 @@
+// contoh kasus
+class {}
+
+class Order {
+    calculateTotalSum() { /* ... */
+    }
+
+    getItems() { /* ... */
+
+    }
+getItemCount() { ?* ... */
+}
+
+addItem(item) { /* ... */
+}
+
+deleteItem(item) { /* ... */
+}
+
+printOrder() { /* ... */
+}
+
+ showOrder() { /* ... */
+}
+
+getDailyHistory() { /* ... */
+}
+
+getMonthHistory() { /* ... */
+}
+}
+
+const main = () => {
+    const order = new Order();
+    order.calculateTotalSum();
+};
+
+main();
