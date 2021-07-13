317da4d87b82921d70d72be1df0dd5c92ca013ba
puteri husnul k <husnulputeri@gmail.com>
2021-07-14 05:44:52 +0700

    Update closePrinciple-7.js

--- open/closePrinciple-7.js
+++ open/closePrinciple-7.js
@@ -1,4 +1,12 @@
 // contoh jenis cinema baru yg brnama couplecinema, maka:
+class CoupleCinema extends Cinema {
+    calculateAdminFee() {
+      return (this.price * 15) / 100;
+    }
+  }
+
+
+// jika ingin membuat class cinema maka contoh kode programnya adalah seperti berikut:
 const main = () => {
     const standardCinema = new StandardCinema(100);
     const adminFee = standardCinema.calculateAdminFee();
